87bdb048484ae5c8ed2a8f56885b9317f5941559
Fourat <f.thamri@instadeep.com>
2022-04-27 16:44:58 +0100

    Add 3 step model pipeline code with random forest

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 # Put the code for your API here.
+import argparse
+from src import clean_step, train_step, slice_eval
+import logging
+
+
+def go(args):
+    """
+    Execute the pipeline
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    if args.action == "all" or args.action == "clean_data":
+        logging.info("clean data")
+        clean_step.clean_data()
+
+    if args.action == "all" or args.action == "train_test_model":
+        logging.info("train and test rf model")
+        train_step.main()
+
+    if args.action == "all" or args.action == "evaluate_slice_performance":
+        logging.info("get model metrics for specific data slices")
+        slice_eval.main()
+
+
+if __name__ == "__main__":
+    """
+    Main entrypoint
+    """
+    parser = argparse.ArgumentParser(description="ML training pipeline")
+
+    parser.add_argument(
+        "--action",
+        type=str,
+        choices=["clean_data",
+                 "train_test_model",
+                 "evaluate_slice_performance",
+                 "all"],
+        default="all",
+        help="Pipeline action"
+    )
+
+    args = parser.parse_args()
+
+    go(args)
--- model_card_template.md
+++ model_card_template.md
@@ -4,15 +4,29 @@ For additional information see the Model Card paper: https://arxiv.org/pdf/1810.
 
 ## Model Details
 
+Fourat Thamri created the model. It is a Random Forest classifier using the default hyperparameters in scikit-learn.
+
 ## Intended Use
+
+This model should be used to predict the salary of a person based on data about its financials.
 
 ## Training Data
 
+The traning data comes from https://archive.ics.uci.edu/ml/datasets/census+income (80% of the full data)
+
 ## Evaluation Data
 
+The testing data comes also from https://archive.ics.uci.edu/ml/datasets/census+income ; (20% of the full data)
+
 ## Metrics
-_Please include the metrics used and your model's performance on those metrics._
+
+Model evaluation was done using the accuracy metric which is mostly around 0.80
 
 ## Ethical Considerations
 
+The dataset contains race, gender and origin country related data. This will result to a model that may be biased toward specific classes.
+Careful investigation of the model bias should be done beforehand.
+
 ## Caveats and Recommendations
+
+
